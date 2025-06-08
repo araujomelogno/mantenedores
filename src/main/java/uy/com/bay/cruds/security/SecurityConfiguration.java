0d8347453d8bd0fe8f67e5c450131683062886b5
German Araujo <germanaraujo@Germans-MacBook-Pro.local>
2025-06-08 17:53:06 -0300

    --

--- src/main/java/uy/com/bay/cruds/security/SecurityConfiguration.java
+++ src/main/java/uy/com/bay/cruds/security/SecurityConfiguration.java
@@ -1,24 +1,25 @@
 package uy.com.bay.cruds.security;
 
-import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.vaadin.flow.spring.security.VaadinWebSecurity;
+
 import uy.com.bay.cruds.views.login.LoginView;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends VaadinWebSecurity {
 
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return NoOpPasswordEncoder.getInstance();
-//    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
